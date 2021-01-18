f9cff80771f76e30268b3eb94b98b51698463c81
ardourApeX <joygupta501@gmail.com>
2021-01-18 23:32:39 +0530

    Bug : Rectified empty user input

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ var outuptDiv = document.querySelector("#output-box");
 var serverURL = "http://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
 
+function isntEmpty(value){
+    
+    if(value.length==0){
+        console.log(value); 
+        return true;
+    }
+    return false
+}
 function errorHandler(error) {
     console.log("Something went wrong - ", error);
     alert("Something went wrong!");
@@ -17,7 +25,10 @@ function getTranslateURL(text){
 function clickEventHandler() {
 
     var inputText = txtInput.value;
-    
+    if(isntEmpty(inputText)){
+        alert("Input field is empty. Kindly enter some text!")
+        return
+    }
     //API Call
     fetch(getTranslateURL(inputText))
     .then(response => response.json())
