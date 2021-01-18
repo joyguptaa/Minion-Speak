09ba7deee443f030dc9386ffc78f0e9be2f6882c
ardourApeX <joygupta501@gmail.com>
2021-01-18 23:59:55 +0530

    test : Tested on minion API

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var btnTranslate = document.querySelector("#btn-translater");
 var txtInput = document.querySelector("#txt-input");
 var outuptDiv = document.querySelector("#output-box");
 
-var serverURL = "http://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// var serverURL = "http://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 
 function isntEmpty(value){
