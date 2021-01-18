8e9204bfb217e40b2de19f11d18796275a4d41ac
ardourApeX <joygupta501@gmail.com>
2021-01-18 23:21:02 +0530

    Style : Style and processing part complete

--- app.js
+++ app.js
@@ -2,8 +2,29 @@ var btnTranslate = document.querySelector("#btn-translater");
 var txtInput = document.querySelector("#txt-input");
 var outuptDiv = document.querySelector("#output-box");
 
+var serverURL = "http://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+
+function errorHandler(error) {
+    console.log("Something went wrong - ", error);
+    alert("Something went wrong!");
+}
+
+function getTranslateURL(text){
+    return serverURL + "?text=" + text;
+}
+
 function clickEventHandler() {
-    outuptDiv.innerText = txtInput.value + "Translated";
+
+    var inputText = txtInput.value;
+    
+    //API Call
+    fetch(getTranslateURL(inputText))
+    .then(response => response.json())
+    .then(json => {var translatedText = json.contents.translated;
+                   outuptDiv.innerText = translatedText })
+    .catch(errorHandler);
+
 };
 
 btnTranslate.addEventListener("click", clickEventHandler); 
